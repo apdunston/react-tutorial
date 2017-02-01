3ff5074c0d4d21c4268f3779f06e6f5bdceae40e
Adrian Dunston <adunston@netsertive.com>
2017-02-01 15:24:15 -0500

    Just talking about Adrian.

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -75,6 +75,7 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
+        <h1>Hypothesis: Adrian is so amazing, you are amazed right now just reading this.</h1>
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
         <CommentForm onCommentSubmit={this.handleCommentSubmit} />
